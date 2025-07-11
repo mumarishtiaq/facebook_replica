1917107de116a07e788a944fb35734e21ae122d5
mumarishtiaq <104498678+mumarishtiaq@users.noreply.github.com>
2025-07-11 12:14:33 -0700

    Completed Posts view and template - will create dummy data for posts

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -5,49 +5,76 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
-
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
-
 
   @override
   State<HomeView> createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<Map<String, String>> posts = [
+    {
+      "avatar": "assets/images/myPic.jpg",
+      "name": "Ali Raza",
+      "time": "2h ago",
+      "image": "assets/images/posts/beach.jpg",
+      "caption": "Salt in the air, sand in my hair—vacation mode activated🌤️",
+      "likesCount": "120",
+    },
+    {
+      "avatar": "assets/images/myPic.jpg",
+      "name": "Ali Raza",
+      "time": "2h ago",
+      "image": "assets/images/posts/beach.jpg",
+      "caption": "Salt in the air, sand in my hair—vacation mode activated🌤️",
+      "likesCount": "120",
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor:AppColors.white,
+    return Scaffold(
+      backgroundColor: AppColors.white,
       body: SafeArea(
-        child:SingleChildScrollView(
-          child: Column(children: [
-        Padding(padding: EdgeInsets.symmetric(horizontal: 16 , vertical: 2),
-        child: 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "facebook",
+                      style: GoogleFonts.roboto(
+                        color: AppColors.primaryBlue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                        letterSpacing: 1,
+                      ),
+                    ),
 
-             Text("facebook", 
-              style: GoogleFonts.roboto(color: AppColors.primaryBlue , fontWeight: FontWeight.bold , fontSize: 32 ,letterSpacing: 1),
+                    Row(
+                      children: [
+                        _buildIconButton(
+                          Icons.add_box_outlined,
+                          false,
+                          0xFF9E9E9E,
+                        ),
+                        SizedBox(width: 7),
+                        _buildIconButton(
+                          Icons.search_rounded,
+                          true,
+                          0xFF9E9E9E,
+                        ),
+                        SizedBox(width: 7),
+                        _buildMessengerIcon(),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-
-              Row(
-                children: [
-                  _buildIconButton(Icons.add_box_outlined,false,0xFF9E9E9E),
-                  SizedBox(width: 7,),
-                  _buildIconButton(Icons.search_rounded,true,0xFF9E9E9E),
-                  SizedBox(width: 7,),
-                  _buildMessengerIcon(),
-
-
-                ],
-              ),
-          ],
-        )
-        ),
-        //building navigation icons
+              //building navigation icons
               _buildNavigationBar(),
 
               //building input row
@@ -55,166 +82,160 @@ class _HomeViewState extends State<HomeView> {
 
               buildStoriesList(),
 
-              // buildPostList(),
-              buildPostListOld(),
-             
-
-
-            
-
-
-
-
-
-      ],)
+              buildPostList(posts),
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
- int _selectedIndex = 0;
 
+  int _selectedIndex = 0;
 
-//custon methods 
-  Widget _buildIconButton(IconData icon , bool isbackground , int col)
-{
-  return Container(
-    padding: EdgeInsets.all(5),
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-      color: isbackground ? AppColors.lightGrey : AppColors.white
-    ),
-    child: Icon(icon, color:  Color(col) , size: 30,),
-  );
-}
-
-Widget _buildMessengerIcon()
-{
-  return Stack(
-    clipBehavior: Clip.none,
-    children: [
-      _buildIconButton(FontAwesomeIcons.facebookMessenger,true,0xFF757575),
-
-      Positioned(right: -6 , top: -6,
-        child: Container(
-          padding: EdgeInsets.all(5),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: AppColors.red,
-            border: Border.all(color: AppColors.white , width: 2)
-          ),
-          child: Text("3" , style: GoogleFonts.roboto(fontSize: 12 , color: AppColors.white),),
-        )
+  //custon methods
+  Widget _buildIconButton(IconData icon, bool isbackground, int col) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: isbackground ? AppColors.lightGrey : AppColors.white,
       ),
-    ],
-  );
-}
+      child: Icon(icon, color: Color(col), size: 30),
+    );
+  }
 
-Widget _buildNavigationBar()
-{
- return Container(
-  padding: EdgeInsets.symmetric(horizontal: 3),
-  decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          bottom: BorderSide(color: AppColors.borderGrey , width: 1)
-        )
-        
-      ),
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-
-      _buildNavigationButton(0,Icons.home_outlined, 36),
-      
-      _buildNavigationButton(1,Icons.ondemand_video_rounded ,30),
-      _buildNavigationButton(2,Icons.group_outlined,33),
-      _buildNavigationButton(4,Icons.store,33),
-      _buildNavigationButton(3,Icons.notifications_outlined,33),
-      _buildNavigationButton(5,Icons.menu,33),
-    ],
-  ),
- );
-}
-
-Widget _buildNavigationButton(int index,IconData icon , double iconSize)
-{
-  bool isSelected = index == _selectedIndex;
-   return Column(
-    children: [
-   IconButton(
-    onPressed: () { _iconOnPress(index);},
-   icon: Icon(icon , color: isSelected ? AppColors.primaryBlue : AppColors.black87 , size: iconSize,),
-   ),
-
-   Container(
-    height: 3,width: 60,
-    decoration: BoxDecoration(
-      color: isSelected? AppColors.primaryBlue : Colors.transparent,
-      borderRadius: BorderRadius.circular(2)
-    ),
-   )
-    ]
-   );
-}
-
-
-
-void _iconOnPress(int index)
-{
- setState(() {
-   _selectedIndex = index;
- });
-}
-
-Widget _buildInputRow()
-{
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-    decoration: BoxDecoration(
-      color: AppColors.white,
-      border: Border(
-        bottom: BorderSide(color: AppColors.borderGrey , width: 5)
-      )
-    ),
-    child: Row(
+  Widget _buildMessengerIcon() {
+    return Stack(
+      clipBehavior: Clip.none,
       children: [
-         CircleAvatar(backgroundImage: AssetImage('assets/images/myPic.jpg'), radius: 22,),
+        _buildIconButton(FontAwesomeIcons.facebookMessenger, true, 0xFF757575),
 
-         SizedBox(width: 10,),
-
-         Expanded(
-          child: Container(height: 40,
-            child: OutlinedButton(onPressed: () {},
-            style: OutlinedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: AppColors.borderGrey
-                ),
-                borderRadius: BorderRadius.circular(25)
-
-              )
-            ), 
-            child: Align( alignment: Alignment.centerLeft,
-              child: Text("What's on your mind?" ,
-            style: TextStyle(color: AppColors.black87 , fontSize: 20),),
-            )
-            
+        Positioned(
+          right: -6,
+          top: -6,
+          child: Container(
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppColors.red,
+              border: Border.all(color: AppColors.white, width: 2),
+            ),
+            child: Text(
+              "3",
+              style: GoogleFonts.roboto(fontSize: 12, color: AppColors.white),
             ),
           ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNavigationBar() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 3),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(color: AppColors.borderGrey, width: 1),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildNavigationButton(0, Icons.home_outlined, 36),
+
+          _buildNavigationButton(1, Icons.ondemand_video_rounded, 30),
+          _buildNavigationButton(2, Icons.group_outlined, 33),
+          _buildNavigationButton(4, Icons.store, 33),
+          _buildNavigationButton(3, Icons.notifications_outlined, 33),
+          _buildNavigationButton(5, Icons.menu, 33),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavigationButton(int index, IconData icon, double iconSize) {
+    bool isSelected = index == _selectedIndex;
+    return Column(
+      children: [
+        IconButton(
+          onPressed: () {
+            _iconOnPress(index);
+          },
+          icon: Icon(
+            icon,
+            color: isSelected ? AppColors.primaryBlue : AppColors.black87,
+            size: iconSize,
+          ),
+        ),
+
+        Container(
+          height: 3,
+          width: 60,
+          decoration: BoxDecoration(
+            color: isSelected ? AppColors.primaryBlue : Colors.transparent,
+            borderRadius: BorderRadius.circular(2),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _iconOnPress(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _buildInputRow() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        border: Border(
+          bottom: BorderSide(color: AppColors.borderGrey, width: 5),
+        ),
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/images/myPic.jpg'),
+            radius: 22,
           ),
 
+          SizedBox(width: 10),
 
-         SizedBox(width: 20,),
+          Expanded(
+            child: Container(
+              height: 40,
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: AppColors.borderGrey),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "What's on your mind?",
+                    style: TextStyle(color: AppColors.black87, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
 
-         IconButton(onPressed: () {}, 
-         icon: Icon(Icons.image, color: Colors.green)
-         ),
-         SizedBox(width: 5,),
+          SizedBox(width: 20),
 
-      ],
-    )
-  );
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.image, color: Colors.green),
+          ),
+          SizedBox(width: 5),
+        ],
+      ),
+    );
+  }
 }
-
-}
-
--- lib/posts_view.dart
+++ lib/posts_view.dart
@@ -1,134 +1,130 @@
-
 import 'package:facebook_replica/Common/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Widget buildPostListOld()
-{
-  return Container(
-      color: Colors.white,
-      padding: const EdgeInsets.symmetric(vertical: 10),
-    
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-
-          // Header
-          ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage("assets/user_profile.jpg"), // Replace with your local asset
-            ),
-            title: Text("Umar Ishtiaq", style: TextStyle(fontWeight: FontWeight.bold)),
-            subtitle: Row(
-              children: [
-                Text("Jul 3 · "),
-                Icon(Icons.public, size: 14, color: Colors.grey),
-              ],
-            ),
-            trailing: Icon(Icons.more_horiz),
-          ),
-
-          // Post Image
-          Image.asset(
-            "assets/images/myPic.jpg", // Replace with your local image
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-
-          // Reaction Row
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            child: Row(
-              children: [
-                Icon(Icons.thumb_up, color: Colors.blue, size: 18),
-                SizedBox(width: 4),
-                Text("1k"),
-              ],
-            ),
-          ),
-
-          // Divider(),
-
-          // Action Buttons
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _PostAction(icon: Icons.thumb_up_alt_outlined, label: "Like"),
-                _PostAction(icon: Icons.comment_outlined, label: "Comment"),
-                _PostAction(icon: FontAwesomeIcons.whatsapp, label: "Send"),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-
-Widget buildPostList()
-{
+Widget buildPostList(List posts) {
   return ListView.builder(
     shrinkWrap: true,
     physics: NeverScrollableScrollPhysics(),
-    itemCount: 20,
-    itemBuilder: (context , index) {
-      return _buildPostTemplate("assets/images/myPic.jpg","Umar Ishtiaq");
-    }
-    );
+    itemCount: posts.length,
+    itemBuilder: (context, index) {
+      final post = posts[index];
+      return _buildPostTemplate(
+        post["avatar"],
+        post["name"],
+        post["time"],
+        post["image"],
+        post["caption"],
+        post["likesCount"],
+      );
+    },
+  );
 }
 
-Widget _buildPostTemplate(String avatar , String name)
-{
-  
-    return Container(
-    padding: EdgeInsets.symmetric(vertical: 10 ),
+Widget _buildPostTemplate(
+  String avatar,
+  String name,
+  String time,
+  String image,
+  String caption,
+  String likesCount,
+) {
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 10),
     width: double.infinity,
     decoration: BoxDecoration(
       color: AppColors.white,
-      border: Border(bottom: BorderSide(color: AppColors.borderGrey , width: 5))
+      border: Border(bottom: BorderSide(color: AppColors.borderGrey, width: 5)),
     ),
     child: Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         ListTile(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage(avatar),
+          leading: CircleAvatar(backgroundImage: AssetImage(avatar)),
+          title: Text(
+            name,
+            style: GoogleFonts.roboto(
+              fontWeight: FontWeight.bold,
+              color: AppColors.black87,
+              fontSize: 16,
+            ),
           ),
-          title:  Text(name , style: GoogleFonts.roboto(fontWeight: FontWeight.bold , color: AppColors.black87 , fontSize: 16),),
-          subtitle: Text("2d" , style: GoogleFonts.roboto(color: AppColors.iconGrey , fontSize: 14)),
-          // trailing: Row(
-          //   children: [
-          //     Icon(Icons.more_horiz),
-          //     Icon(Icons.close),
+          subtitle: Row(
+            children: [
+              Text(
+                time,
+                style: GoogleFonts.roboto(
+                  color: AppColors.iconGrey,
+                  fontSize: 14,
+                ),
+              ),
+              SizedBox(width: 10),
+              Icon(Icons.public, size: 18, color: AppColors.black87),
+            ],
+          ),
+          trailing: SizedBox(
+            width: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.more_horiz, size: 30, color: AppColors.iconGrey),
+                SizedBox(width: 20),
+                Icon(Icons.close, size: 30, color: AppColors.iconGrey),
+              ],
+            ),
+          ),
+        ),
 
-          //   ],
-          // ),
-          trailing:  Icon(Icons.more_horiz),
-        )
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              caption,
+              textAlign: TextAlign.left,
+              style: GoogleFonts.roboto(color: Colors.black, fontSize: 16),
+            ),
+          ),
+        ),
+
+        Image.asset(image, width: double.infinity, fit: BoxFit.cover),
+
+        // Reaction Row
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          child: Row(
+            children: [
+              Icon(Icons.thumb_up, color: AppColors.primaryBlue, size: 18),
+              SizedBox(width: 3),
+              Icon(Icons.heart_broken_rounded, color: Colors.pink, size: 18),
+              SizedBox(width: 4),
+              Text(likesCount),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildPostActionIcons(Icons.thumb_up_alt_outlined, "Like"),
+              _buildPostActionIcons(Icons.comment_outlined, "Comment"),
+              _buildPostActionIcons(FontAwesomeIcons.whatsapp, "Send"),
+            ],
+          ),
+        ),
       ],
     ),
-
   );
 }
 
-
-class _PostAction extends StatelessWidget {
-  final IconData icon;
-  final String label;
-
-  const _PostAction({required this.icon, required this.label});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(icon, size: 20, color: Colors.grey[700]),
-        SizedBox(width: 4),
-        Text(label, style: TextStyle(color: Colors.grey[700])),
-      ],
-    );
-  }
+Widget _buildPostActionIcons(IconData icon, String label) {
+  return Row(
+    children: [
+      Icon(icon, size: 20, color: AppColors.iconGrey),
+      SizedBox(width: 4),
+      Text(label, style: GoogleFonts.roboto(color: AppColors.iconGrey)),
+    ],
+  );
 }
